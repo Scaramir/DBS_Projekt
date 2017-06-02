99cc62a7dbe862d6b6874eb79033f49c1f40253b
Acedon95 <lackyluck@hotmail.de>
2017-06-02 13:59:41 +0200

    Add files via upload

---
+++ DataImport.py
@@ -0,0 +1,13 @@
+
+import psycopg2
+#Läd die Daten über MYSQL nach Verbindung mit der Datenbank
+#über eine SQL Querie in die election Datenbank
+#der Cursor ermöglicht die connection und execute führt die jeweiligen
+#Sql Statements aus!
+connection = psycopg2.Connect(host='localhost', user='kikou', passwd='hallo123', db='election')
+cursor = connection.cursor()
+query1 = "LOAD DATA LOW_PRIORITY LOCAL INFILE 'C:/Users/acedon/Desktop/new-american-election.csv' INTO TABLE tweets CHARACTER SET ascii FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\n' IGNORE 1 LINES (`TEXT`, `REPLY_TO`, `DATUM`, `is_quote`, `RETWEET_CNT`, `LIKE_CNT`, `ID`, `truncated`);"
+query2 = "LOAD DATA LOW_PRIORITY LOCAL INFILE 'C:/Users/acedon/Desktop/hashtags.csv' INTO TABLE hashtag CHARACTER SET ascii FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\n' (`HASHTAGS`, `ID`);"
+cursor.execute( query1 )
+cursor.execute( query2 )
+connection.commit()
