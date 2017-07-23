a0e21aaec241d560b794eb02d06fade46fc6ed36
maximilian45 <maxotto45@gmail.com>
2017-07-23 20:12:13 +0200

    Update Datenbereinigung.py

--- Datenbereinigung.py
+++ Datenbereinigung.py
@@ -10,15 +10,12 @@ reader = csv.reader(in_file,delimiter=";")
 writer = csv.writer(out_file,delimiter=";")
 
 for row in reader:
-    #row[1] = row[1].replace(";",",")#ersetzt evtl. auftauchende ";", um frühzeitige Zelltrennung zu vermeiden
+    row[1] = row[1].replace(";",",")#ersetzt evtl. auftauchende ";", um frühzeitige Zelltrennung zu vermeiden
     row[2] = row[2].replace("True","1")#wandelt die boolschen Werte von True/False zu 1 und 0
     row[2] = row[2].replace("False","0")
     row[4] = row[4].replace("T"," ")#Entfernt das T zum einlesen des Datums
     if row[10] != "True":#Entfernt gekürzte tweeets
-                        writer.writerow([row[0],row[1],row[2],row[3],row[4],row[5],row[7],row[8]])# Entfernt überflüssige Rows die nicht in der Datenbank benötig werden
+                        writer.writerow([row[0],row[1],row[2],row[3],row[4],row[5],row[7],row[8]])#schreibt alle ungekürzten in die out_file
         
 in_file.close()
 out_file.close()
-
-		
-			
